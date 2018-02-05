91a3b66ed36816518d6bd982c0f02eafdfbe1346
sandhiyakarthicsaran <35221190+sandhiyakarthicsaran@users.noreply.github.com>
2018-02-05 15:57:00 +0530

    positivenegative
    positive negative

--- pos.C
+++ pos.C
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("Enter the value:\n",a);
+  scanf("%d",&a);
+  if(a<0)
+  {
+    printf("The value %d is negative!!!\n",a);
+  }
+  if(a>0)
+  {
+    printf("The value %d is positive!!!\n",a);
+  }
+  if(a==0)
+  {
+    printf("The number is zero!!!!\n",a);
+  }
+  return 0;
+}
